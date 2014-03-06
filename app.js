54ae9b9bf23a676d915be9773689b4abe8f2d9d1
Dan Hable <dhable@gmail.com>
2014-03-05 20:53:40 -0800

    adding in bootstrapping logic to the server

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+ * Bullhorn: Get your message heard
+ *
+ * Copyright (C) 2014 jetway.io. All rights reserved.
+ */
+var conf = require("./lib/conf.js"),
+    logger = require("./lib/logger.js"),
+    server = require("./lib/server.js");
+
+
+// Log a preamble header to the logging system as a test of the
+// logging system and to show in the logs where a fresh state occured.
+var log = logger("app");
+log.info("Bootstrapping jetway.io Bullhorn, version 0.1.0");
+
+
+// Create new instances of the services that will be running to handle
+// requests.
+var port = conf().get("port"),
+    api = server();
+
+api.listen(port, function() {
+  log.info("API is listening on port %s", port);
+});
---
+++ spec/placeholder.js
@@ -0,0 +1,6 @@
+
+describe("In the placeholder spec", function() {
+  it("boolean true should be boolean true.", function() {
+    expect(true).toBe(true);
+  });
+});
