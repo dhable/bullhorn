2b35490cbd07768fbf039fc17848fec844b4612e
Dan Hable <dhable@gmail.com>
2014-06-03 12:19:07 -0700

    Cleaning up grunt
       1. Added grunt time plugin so we can see where the code base is
          starting to slow down.
    
       2. Removed YUI plugin. Eventually, we'll switch to JSDoc for internal
          documentation and RAML for external documentation.

--- gruntfile.js
+++ gruntfile.js
@@ -1,35 +1,25 @@
 module.exports = function(grunt) {
   "use strict";
   require("load-grunt-tasks")(grunt);
+  require("time-grunt")(grunt);
+
+
+  var source = ["*.js", "lib/**/*.js", "spec/**/*.js", "package.json", "conf/*.json"];
   
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      src: ["*.js", "lib/**/*.js", "spec/**/*.js",
-            "package.json", "conf/*.json"]
+      src: source
     },
     jasmine_node: {
       all: ["spec/"]
     },
     clean: {
       testOutput: ["_SpecRunner.html", "*.log"],
-      generatedDocs: ["docs/api"]
-    },
-    yuidoc: {
-      compile: {
-        name: '<%= pkg.name %>',
-        description: '<%= pkg.description %>',
-        version: '<%= pkg.version %>',
-        url: '<%= pkg.homepage %>',
-        options: {
-          paths: 'lib/',
-          outdir: 'docs/api'
-        }
-      }
+      generatedDocs: ["docs/bullhorn"]
     }
   });
 
-  grunt.registerTask("setup", ["yuidoc"]);
   grunt.registerTask("test", ["jshint", "jasmine_node", "clean:testOutput"]);
   grunt.registerTask("release", ["test", "validate-package"]);
 
