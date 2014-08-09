04f1ac97419021316e354f8258dc42a50b394d3f
Dan Hable <dhable@gmail.com>
2014-08-09 12:56:34 -0700

    Fixing authentication logic
    The API looks bad when everything is rooted on the domainId. Instead, we encode everything for auth in the header. This change:
    
      1. Updates the restify hander to pull apart the auth header and use the values for authentication.
    
      2. Adds a new security helper function to parse the header value format.
    
      3. Associated tests.

--- spec/securitySpec.js
+++ spec/securitySpec.js
@@ -5,37 +5,36 @@ describe("The security module", function() {
    var salt = "DEVONLY";
 
    describe("generateKey() function", function() {
-      it("should return a base64 hash given a string appId", function() {
+      it("should return a base64 hash given a string accessKeyId", function() {
           var actualHash = security.generateKey(salt, "af38c556-7653-4c41-8f1e-81ee859a817c");
           expect(actualHash).toBeDefined();
           expect(function() { new Buffer(actualHash, "base64"); }).not.toThrow();
       });
 
-      it("should raise an exception if appId is null or undefined", function() {
+      it("should raise an exception if accessKeyId is null or undefined", function() {
          expect(function() { security.generateKey(salt, null); }).toThrow();
          expect(function() { security.generateKey(salt, undefined); }).toThrow(); 
       });
 
-   });
-
+   }); // end of generateKey() function
 
 
    describe("isKeyValid() function", function() {
-      var testAppId = "af38c556-7653-4c41-8f1e-81ee859a817c",
-          testApiKey = security.generateKey(salt, testAppId);
+      var testAccessKeyId = "af38c556-7653-4c41-8f1e-81ee859a817c",
+          testApiKey = security.generateKey(salt, testAccessKeyId);
 
       it("should return false if the apiKey is null", function() { 
-         var isValid = security.isKeyValid(salt, testAppId, null);
+         var isValid = security.isKeyValid(salt, testAccessKeyId, null);
          expect(isValid).toBe(false);
       });
 
       it("should return false if the apiKey is undefined", function() {
-         var isValid = security.isKeyValid(salt, testAppId, undefined);
+         var isValid = security.isKeyValid(salt, testAccessKeyId, undefined);
          expect(isValid).toBe(false);
       });
 
       it("should return true if the apiKey matches", function() {
-         var isValid = security.isKeyValid(salt, testAppId, testApiKey);
+         var isValid = security.isKeyValid(salt, testAccessKeyId, testApiKey);
          expect(isValid).toBe(true);
       });
 
@@ -44,17 +43,57 @@ describe("The security module", function() {
          expect(isValid).toBe(false);
       });
 
-      it("should return false is the appId is null", function() {
+      it("should return false is the accessKeyId is null", function() {
          var isValid = security.isKeyValid(salt, null, testApiKey);
          expect(isValid).toBe(false);
       });
 
-      it("should return false is the appId is undefined", function() {
+      it("should return false is the accessKeyId is undefined", function() {
          var isValid = security.isKeyValid(salt, undefined, testApiKey);
          expect(isValid).toBe(false);
       });
+   }); // end of isKeyValid() function
 
-   });
 
+   describe("isAuthenticationHeaderValid() function", function() {
+      var testAuthHeaderValue = "94f995a0-1ff9-11e4-8734-57e30f75f87a=x4Lwm6vETwKuc7D9m3Kfcn47nb7UM4AXw39dvJyZmCcwSrt4a/yruDZ2I7WPvENq1xu8A4pUXAR3ke6G2iZKrA==",
+          testBadAuthHeaderValue1 = "94f995a0-1ff9-11e4-8734-57e30f75f87a x4Lwm6vETwKuc7D9m3Kfcn47nb7UM4AXw39dvJyZmCcwSrt4a/yruDZ2I7WPvENq1xu8A4pUXAR3ke6G2iZKrA==";
+
+      it("should return true when header is valid", function() {
+         var isValid = security.isAuthenticationHeaderValid(salt, testAuthHeaderValue);
+         expect(isValid).toBe(true);
+      });
+
+      it("should return false when header is undefined", function() {
+         var isValid = security.isAuthenticationHeaderValid(salt, undefined);
+         expect(isValid).toBe(false);
+      });
+
+      it("should return false when header is null", function() {
+         var isValid = security.isAuthenticationHeaderValid(salt, null);
+         expect(isValid).toBe(false);
+      });
+
+      it("should return false when header doesn't contain = between accessKeyId and secret", function() {
+         var isValid = security.isAuthenticationHeaderValid(salt, testBadAuthHeaderValue1);
+         expect(isValid).toBe(false);
+      });
+
+      it("should return false when the salt is undefined", function() {
+         var isValid = security.isAuthenticationHeaderValid(undefined, testAuthHeaderValue);
+         expect(isValid).toBe(false);
+      });
+
+      it("should return false when the salt is null", function() {
+         var isValid = security.isAuthenticationHeaderValid(null, testAuthHeaderValue);
+         expect(isValid).toBe(false);
+      });
+
+      it("should return false when the salt does not match the key", function() {
+         var isValid = security.isAuthenticationHeaderValid("someOtherSalt", testAuthHeaderValue);
+         expect(isValid).toBe(false);
+      });
+
+   }); // end of isAuthenticationHeaderValid() function
 
 });
