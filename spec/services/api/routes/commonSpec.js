427e12ee2300ed4731db5c10d42dee6ee8d3e7ab
Dan Hable <dhable@gmail.com>
2014-08-09 14:19:04 -0700

    Fixed authroize restify handler
     1. Fixed handler to now get and set req.local.accessKeyId from Authorization header.
    
     2. Does authentication against same header.
    
     3. Added unit test. Could use some more work but getting there.

---
+++ spec/services/api/routes/commonSpec.js
@@ -0,0 +1,30 @@
+var common = require("../../../../lib/services/api/routes/common.js");
+
+
+describe("The common API route module", function() {
+    var mockLog = jasmine.createSpyObj("log", ["trace", "debug", "info", "warn", "error"]);
+
+    describe("authorize() restify handler", function() {
+        it("", function() {
+            var mockConf = {
+                    get: function() {
+                        return "DEVONLY";
+                    }
+                }, 
+                mockNext = jasmine.createSpy("next"),
+                response = {},
+                request = {
+                    local: {},
+                    authorization: {
+                        scheme: "X-Jetway-API-Key",
+                        credentials: "94f995a0-1ff9-11e4-8734-57e30f75f87a=x4Lwm6vETwKuc7D9m3Kfcn47nb7UM4AXw39dvJyZmCcwSrt4a/yruDZ2I7WPvENq1xu8A4pUXAR3ke6G2iZKrA=="
+                    }
+                };
+            
+            common(mockLog, mockConf).authorize(request, response, mockNext);
+            expect(mockNext).toHaveBeenCalled();
+            expect(request.local.accessKeyId).toBe("94f995a0-1ff9-11e4-8734-57e30f75f87a");
+        });
+    }); // end of authorize() restify handler
+
+});
